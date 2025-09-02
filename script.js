120bac603f570308d56792476f13e09df1f8c074
jaspionU2 <138932022+jaspionU2@users.noreply.github.com>
2025-09-02 19:40:33 +0000

    metodo get adicionado

--- script.js
+++ script.js
@@ -7,24 +7,32 @@ const port = 3000
 
 app.use(express.json());
 
+const listaPessoas = []
+
+app.get('/getPeople', (req, res) => {
+    res.json(listaPessoas)
+    console.log(listaPessoas)
+})
+
 app.post('/NewPeople/:cep', async (req, res) => {
     const person = req.body
     const cep = req.params.cep
     const address = await getAddress(cep)
 
-    const newperson = new Pessoa(
+    const newPerson = new Pessoa(
         person.nome,
         person.idade,
         person.email,
         address
     )
 
+    listaPessoas.push(newPerson)
+
     res.json({
-        newperson
+        newPerson
     })
 })
 
-
-app.listen(3000, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta 3000`)
 })
